e520ba7808bd38ec5acd4af6c8a90cb13d0b4977
trungdung70199 <trungdung70199@gmail.com>
2023-12-04 12:21:33 +0900

    update item request

--- app/Http/Requests/ItemRequest.php
+++ app/Http/Requests/ItemRequest.php
@@ -23,11 +23,19 @@ class ItemRequest extends FormRequest
     public function messages(): array
     {
         return [
-            'name.required' => "商品名を入力してください",
-            'price.required' => "価格を入力してください",
-            'price.integer' => "価格は数値を入力してください",
-            'price.min' => "「0」以上で入力してください",
-            'price.max' => "「100,000」以下で入力してください",
+            // 'name.required' => "商品名を入力してください",
+            // 'price.required' => "価格を入力してください",
+            // 'price.integer' => "価格は数値を入力してください",
+            // 'price.min' => "「0」以上で入力してください",
+            // 'price.max' => "「100,000」以下で入力してください",
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'name' => __('message.item_name'),
+            'price' => __('message.price'),
         ];
     }
 }
